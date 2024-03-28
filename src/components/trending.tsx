fcbd6b97c81a064391efde99fe9a8c4b31685699
Lisec, George <glisec@iu.edu>
2024-03-28 17:06:37 -0400

    api is W

--- src/components/trending.tsx
+++ src/components/trending.tsx
@@ -1,37 +1,46 @@
+import { useEffect, useState } from "react";
+
 export function Movies() {
+  type Movie = {
+    poster_path: string;
+  };
 
+  type MovieApiResponse = {
+    results: Movie[];
+  };
 
-  //Promise<Type> is a generic interface, indicates the type of data the Promise is returning
-  const getMovie = async (): Promise<void> => {
-    try {
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    //Promise<Type> is a generic interface, indicates the type of data the Promise is returning
+    const getMovie = async () => {
+      try {
         //Uses await to pause the execution of async function until the promise returned by fetch is resolved
         //fetch always returns a promise
         //in the future type async functions with Promise<>
-      const response = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key="
-      );
-
-      // that indicates if the HTTP request was successful
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
+        const response = await fetch(
+          import.meta.env.VITE_API_URL
+        );
+        // that indicates if the HTTP request was successful
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        // TypeScript can infer the type of `data` from the `await` expression
+        // When you use the await keyword before an expression that returns a Promise,
+        // TypeScript can "look inside" the Promise and infer the type of the resolved value.
+        // In this case, await response.json() means that TypeScript knows the resolved value
+        // of the Promise is the data returned by response.json(),
+        // which is typically a JavaScript object or array representing the parsed JSON data.
+        const data: MovieApiResponse = await response.json();
+        setMovieList(data.results);
+        console.log(data.results.slice(0, 20))
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-    
-    // TypeScript can infer the type of `data` from the `await` expression
-    // When you use the await keyword before an expression that returns a Promise, 
-    // TypeScript can "look inside" the Promise and infer the type of the resolved value.
-    // In this case, await response.json() means that TypeScript knows the resolved value 
-    // of the Promise is the data returned by response.json(), 
-    // which is typically a JavaScript object or array representing the parsed JSON data.
-      
-      const data = await response.json();
-      console.log(data);
+    };
 
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  getMovie();
+    getMovie();
+  }, []);
 
   //what not to do is below in comments OUTDATED TECH U RAT
   /*
@@ -55,5 +64,14 @@ export function Movies() {
 
       */
 
-  return <div></div>;
+  return (
+    <div>
+      {movieList.slice(0, 5).map((movie) => (
+        <img
+          className="flex w-1/3 h-1/3 m-10"
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        />
+      ))}
+    </div>
+  );
 }
