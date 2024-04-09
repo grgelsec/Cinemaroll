5345fc4d103c1e80ff86576d09422981693b2f09
Lisec, George <glisec@iu.edu>
2024-04-09 12:12:17 -0400

    previews done

--- src/components/previews.tsx
+++ src/components/previews.tsx
@@ -6,6 +6,7 @@ export default function UserFeatures() {
     title: string;
     vote_average: number;
     vote_count: number;
+    genre_ids: number[];
   };
 
   type MovieAPIResponse = {
@@ -35,15 +36,15 @@ export default function UserFeatures() {
   }, []);
 
   return (
-    <div className="flex col flex-wrap justify-center lg:px-40 md:px-40 sm:px-40 space-y-10 ring-8 ring-red-500">
-      <div className="flex col flex-wrap justify-center text-whitePurp font-semibold py-2 ring-4 ring-green-500 w-8/12">
-        What have you watched?
-        <p className="flex flex-wrap justify-center text-center text-whitePurp border-t opacity-70 font-sans font-light left-0 py-3 ring-2 ring-yellow-500 ">
+    <div className="flex col flex-wrap justify-center lg:px-40 md:px-40 sm:px-40 space-y-10">
+      <div className="flex col flex-wrap justify-center text-whitePurp font-semibold py-2 w-8/12">
+        <h1 className="py-2">What have you watched?</h1>
+        <p className="flex flex-wrap justify-center text-center text-whitePurp border-t opacity-70 font-sans font-light left-0 py-3">
           Let us know what you've seen! Cinemaroll allows you to keep track of
           every movie that you've watched. Build your library and keep a running
           log!
         </p>
-        <div className="flex flex-wrap row justify-center gap-3 ring-4  ring-pink-400">
+        <div className="flex flex-wrap row justify-center gap-3">
           {posterList.slice(17, 20).map((movie) => (
             <img
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
@@ -52,14 +53,14 @@ export default function UserFeatures() {
           ))}
         </div>
       </div>
-      <div className="flex col flex-wrap justify-center text-whitePurp font-semibold py-2 ring-4 ring-green-500 w-8/12">
-        What would you rate it?
-        <p className="flex flex-wrap justify-center text-center text-whitePurp border-t opacity-70 font-sans font-light left-0 py-3 ring-2 ring-yellow-500 ">
+      <div className="flex col flex-wrap justify-center text-whitePurp font-semibold py-2 w-8/12">
+       <h1 className="py-2">What would you rate it?</h1>
+        <p className="flex flex-wrap justify-center text-center text-whitePurp border-t opacity-70 font-sans font-light left-0 py-3">
           Rate each film to record how you feel about it! While you're at it, Go
           more in depth on what you loved or hated. No wrong answers...
           hopefully.
         </p>
-        <div className="flex flex-wrap row items-center justify-center gap-3 ring-4  ring-pink-400">
+        <div className="flex flex-wrap row items-center justify-center gap-3 ">
           {posterList.slice(19, 20).map((movie) => (
             <img
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
@@ -107,9 +108,9 @@ export default function UserFeatures() {
           </h1>
         </div>
       </div>
-      <div className="flex col flex-wrap justify-center text-whitePurp font-semibold py-2 ring-4 ring-green-500 w-8/12">
-        Did you love it it?
-        <p className="flex flex-wrap justify-center text-center text-whitePurp border-t opacity-70 font-sans font-light left-0 py-3 ring-2 ring-yellow-500 ">
+      <div className="flex col flex-wrap justify-center text-whitePurp font-semibold py-2 w-8/12">
+        <h1 className="py-2">Did you love it it?</h1>
+        <p className="flex flex-wrap justify-center text-center text-whitePurp border-t opacity-70 font-sans font-light left-0 py-3 ">
           With each film you watch, take the opportunity to show us that you
           loved it! If you really liked it, give the film a 'heart' indicating
           to others what floats your boat.
@@ -130,6 +131,64 @@ export default function UserFeatures() {
             >
               <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
             </svg>
+          </div>
+        </div>
+      </div>
+      <div className="flex col flex-wrap justify-center text-whitePurp font-semibold py-2 w-8/12">
+        <h1 className="py-2">Don't know what to watch?</h1>
+        <p className="flex flex-wrap justify-center text-center text-whitePurp border-t opacity-70 font-sans font-light left-0 py-3">
+          Ever have those times where you don't know what to watch? Lists allow
+          you to search through specfic genres to find what you're looking for.
+          Maybe a classic or sad film, there is a list for what you want.
+        </p>
+        <h1 className="border-b border-mediumPurp font-light">
+          Trending Action
+        </h1>
+        <div className="flex flex-wrap row items-center justify-center gap-3 py-2 ">
+          {posterList.slice(10, 14).map((movie) => (
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              className="flex items-center rounded-xl lg:w-2/12 md:w-4/12 sm:w-5/12 hover:opacity-50 hover:outline-none hover:border-transparent hover:ring-4 hover:ring-indigo-500 transition-sexy mt-3"
+            ></img>
+          ))}
+        </div>
+        <h1 className="border-b border-mediumPurp font-light">Must See</h1>
+        <div className="flex flex-wrap row items-center justify-center gap-3 py-2 ">
+          {posterList.slice(14, 18).map((movie) => (
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              className="flex items-center rounded-xl lg:w-2/12 md:w-4/12 sm:w-5/12 hover:opacity-50 hover:outline-none hover:border-transparent hover:ring-4 hover:ring-indigo-500 transition-sexy mt-3"
+            ></img>
+          ))}
+        </div>
+        <h1 className="border-b border-mediumPurp font-light">Hidden Gems</h1>
+        <div className="flex flex-wrap row items-center justify-center gap-3 py-2 ">
+          {posterList.slice(5, 9).map((movie) => (
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              className="flex items-center rounded-xl lg:w-2/12 md:w-4/12 sm:w-5/12 hover:opacity-50 hover:outline-none hover:border-transparent hover:ring-4 hover:ring-indigo-500 transition-sexy mt-3"
+            ></img>
+          ))}
+        </div>
+      </div>
+      <div className="flex col flex-wrap justify-center text-whitePurp font-semibold py-2 w-8/12">
+        <h1 className="py-2">Have any hot takes?</h1>
+        <p className="flex flex-wrap justify-center text-center text-whitePurp border-t opacity-70 font-sans font-light left-0 py-3 ">
+          Do you have any hills you want to die on? Maybe you 
+          have some wild movie opinions that you'd love to discuss.
+          Hot takes is an open field for discussion and debate about movie
+          opinions that not everyone is going to love.
+        </p>
+        <div className="flex flex-wrap row items-center justify-center py-2 gap-3">
+          {posterList.slice(16, 17).map((movie) => (
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              className="flex items-center rounded-xl lg:w-3/12 md:w-3/12 hover:opacity-50 hover:outline-none hover:border-transparent hover:ring-4 hover:ring-indigo-500 transition-sexy mt-3"
+            ></img>
+          ))}
+          <div className=" py-2 px-10 font-sans font-light text-center rounded-3xl text-lightPurp">
+            "People should stop making these kind of films because they don't
+            resonate with modern audiances. Anyone disagree?"
           </div>
         </div>
       </div>
