081614cda4477e6d8bbe06762e20404da9799a57
Lisec, George <glisec@iu.edu>
2024-04-27 12:18:29 -0400

    films prog

--- src/components/films.tsx
+++ src/components/films.tsx
@@ -10,7 +10,7 @@ export default function Films() {
     vote_count: number;
     genre_ids: number[];
     page: number;
-    popularity: number
+    popularity: number;
   };
 
   type MovieAPIResponse = {
@@ -41,23 +41,26 @@ export default function Films() {
 
   return (
     <>
+      <NavBar></NavBar>
       <div>
-        <NavBar></NavBar>
         <header>
           <div className="flex justify-center py-6">
             <div className="text-white text-3xl font-semibold">Films</div>
           </div>
         </header>
-        <body>
-        <div className="flex flex-wrap row justify-center gap-3 px-40">
-            {filmList.filter(movie => movie.popularity > 1000).map((movie) => (
-              
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                className="flex items-center rounded-xl lg:w-1/12 md:w-2/12 sm:w-2/12 xs:w-1/12 hover:opacity-50 hover:outline-none hover:border-transparent hover:ring-4 hover:ring-indigo-500 transition-sexy mt-3"
-              ></img>
-            ))
-            }
+        <body className="flex col justify-center flex-wrap lg:px-40 md:px-40 sm:px-40 space-y-10 ">
+          <div className="flex col flex-wrap justify-center text-xl ring-4 text-whitePurp ring-red-500">
+            <h1 className="py-2">Popular</h1>
+            <div className="flex col flex-wrap justify-center gap-3 ring-2 ring-white">
+              {filmList
+                .filter((movie) => movie.popularity > 1000)
+                .map((movie) => (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    className="flex items-center rounded-xl lg:w-1/12 md:w-2/12 sm:w-2/12 xs:w-1/12 hover:opacity-50 hover:outline-none hover:border-transparent hover:ring-4 hover:ring-indigo-500 transition-sexy mt-3"
+                  ></img>
+                ))}
+            </div>
           </div>
         </body>
       </div>
