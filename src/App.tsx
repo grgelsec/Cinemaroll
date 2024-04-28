e9ecd76a962dffdef7f5f6e0313adb0150140541
Lisec, George <glisec@iu.edu>
2024-04-28 19:13:37 -0400

    added singin

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from "./components/homepage";
 import Films from "./components/films";
 import { Route, Routes } from "react-router-dom";
 import Create from "./components/createaccount";
+import SignIn from "./components/signin";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route key={'home'} path="/" element={<Home/>}/>
         <Route path="/films" element={<Films/>} />
         <Route path="/createaccount" element={<Create/>} />
+        <Route path="/sign-in" element={<SignIn/>}/>
       </Routes>
     </>
   );
