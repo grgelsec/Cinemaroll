2e64185edd3e0cc0f542a051ca9b2acedd57030e
Lisec, George <glisec@iu.edu>
2024-05-11 14:59:37 -0400

    working on reccomender design

--- src/components/openai-test.ts
+++ src/components/openai-test.ts
@@ -1,19 +1,23 @@
 import OpenAI from "openai";
 
 // This file is me learning how to use OpenAPI
-//TODO: Need Figure out how to get user input in TS and then ask GPT the user input
+//TODO: Need Figure out how to get user input in TS and then ask GPT the user input 
 //TODO: Need to figure out how to use this inside reccomender.tsx
 
 const openai = new OpenAI();
 
+const questions = ["What is color?", "What is a Dog?", "What is a cat?"]
+
 async function main() {
 
     const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "Tell me a joke please?" }],
+        messages: [{ role: "system", content: `${questions[1]}` }],
         model: "gpt-3.5-turbo",
     })
 
-    console.log(completion.choices[0])
+    const messageContent: string | null = completion.choices[0].message.content
+
+    console.log(messageContent)
 }
 
 main();
--- src/components/reccomender.tsx
+++ src/components/reccomender.tsx
@@ -1,9 +1,36 @@
 import NavBar from "./navbar";
 
 export default function Reccomend() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <body className="flex col flex-wrap justify-center px-40 lg:px-40 md:px-40 sm:px-40 ring-2">
+      <div className="flex w-8/12 lg:w-8/12 md:w-8/12 sm:w-8/12 col p-20 ring-2">
 
-
-    return (
-        <NavBar></NavBar>
-    );
+        </div>
+      <div className="">
+        <div className="flex justify-center p-3 font-bold text-white lg:gap-3 md:gap-3 sm:gap-3 ring-2">
+              <button className="py-2 px-2 bg-lightPurp ring-white rounded-xl hover:ring-2 hover:ring-darkPurp hover:bg-mediumPurp transition-sexy ring-2">
+                Action
+              </button>
+              <button className="p-2 bg-lightPurp ring-white rounded-xl hover:ring-2 hover:ring-darkPurp hover:bg-mediumPurp transition-sexy ring-2">
+                Romance
+              </button>
+              <button className="p-2 bg-lightPurp ring-white rounded-xl hover:ring-2 hover:ring-darkPurp hover:bg-mediumPurp transition-sexy ring-2">
+                Comedy
+              </button>
+              <button className="p-2 bg-lightPurp ring-white rounded-xl hover:ring-2 hover:ring-darkPurp hover:bg-mediumPurp transition-sexy ring-2">
+                Drama
+              </button>
+              <button className="p-2 bg-lightPurp ring-white rounded-xl hover:ring-2 hover:ring-darkPurp hover:bg-mediumPurp transition-sexy ring-2">
+                Musical
+              </button>
+              <button className="p-2 bg-lightPurp ring-white rounded-xl hover:ring-2 hover:ring-darkPurp hover:bg-mediumPurp transition-sexy ring-2">
+                Romcom
+              </button>
+        </div>
+      </div>
+      </body>
+    </>
+  );
 }
