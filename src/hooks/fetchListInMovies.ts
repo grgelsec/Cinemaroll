838ee5551e02d57cb506235b60f1d18a88cce374
Lisec, George <glisec@iu.edu>
2024-06-01 20:11:29 -0400

    sike, now its done

---
+++ src/hooks/fetchListInMovies.ts
@@ -0,0 +1,47 @@
+import { error } from "console";
+import { useState, useEffect } from "react";
+
+//each list contains id and name
+type List = {
+  id: string;
+  name: string;
+};
+
+//Array of list ids and names
+type ListAPIResponse = {
+  results: List[];
+};
+
+const useListContainsMovie = (pageNum: number, movieID: string) => {
+
+  const [listInfo, setListInfo] = useState<List[]>();
+
+  useEffect(() => {
+    const getList = async () => {
+
+      const baseUrl = "https://api.themoviedb.org/3/movie/";
+      const api_key = import.meta.env.VITE_API_URL3;
+      
+      try {
+      //input movie id that will be fetched by fetchMovies
+      const response = await fetch(`${baseUrl}${movieID}/lists?api_key=${api_key}`);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+
+      const data: ListAPIResponse = await response.json();
+      setListInfo(data.results)
+      } catch {
+        console.error("Error fetching data", error);
+      }
+
+    };
+
+    getList();
+  }, [pageNum, movieID]);
+
+  return {listInfo}
+};
+
+export default useListContainsMovie;
