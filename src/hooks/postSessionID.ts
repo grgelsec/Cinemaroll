fe48985658e30e435ca7c15651914d0f57501a1a
Lisec, George <glisec@iu.edu>
2024-08-13 17:08:35 -0400

    adding comments

---
+++ src/hooks/postSessionID.ts
@@ -0,0 +1,2 @@
+//need to write a post request that takes in params, this post request will be called in the authorized page
+//need to use useParams to grab the params from the url and put them into the post request to receive the needed sessionID
