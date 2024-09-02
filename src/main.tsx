40e43a58f072746af3f1b79fbf5b7a4ae2c15196
Lisec, George <glisec@iu.edu>
2024-09-02 19:19:32 -0400

    create root fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { SessionProvider } from "./context/SessionContext.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <SessionProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </SessionProvider>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <SessionProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SessionProvider>
+  );
+}
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <SessionProvider>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </SessionProvider>
+// );
