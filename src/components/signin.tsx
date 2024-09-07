02786d08865e691fcd4be173e4a503d1d8d34971
Lisec, George <glisec@iu.edu>
2024-09-07 11:17:44 -0400

    more url stuff

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -24,7 +24,7 @@ export default function SignIn() {
   return (
     <div className="">
       <Link
-        to={`https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${baseURL}/sign-in/auth-page`}
+        to={`https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${baseURL}sign-in/auth-page`}
         className="flex flex-wrap justify-center p-1 md:p-2 lg:p-2 text-xs lg:text-md text-codBlack bg-white font-mono rounded-lg"
       >
         Sign In
