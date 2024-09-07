824da3d8320255fb2fb5be1a0a8538bf08c72985
Lisec, George <glisec@iu.edu>
2024-09-07 10:48:30 -0400

    url stuff

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -19,10 +19,12 @@ export default function SignIn() {
     );
   }
 
+  const baseURL = import.meta.env.VITE_APP_BASE_URL;
+
   return (
     <div className="">
       <Link
-        to={`https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=https://cinemaroll.vercel.app/sign-in/auth-page`}
+        to={`https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${baseURL}/sign-in/auth-page`}
         className="flex flex-wrap justify-center p-1 md:p-2 lg:p-2 text-xs lg:text-md text-codBlack bg-white font-mono rounded-lg"
       >
         Sign In
